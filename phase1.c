8652e15e9554609c640a681db567246eac864256
ArshiaIzd <stevevisht@gmail.com>
2023-01-12 21:14:17 +0330

    cat added!

--- phase1.c
+++ phase1.c
@@ -6,17 +6,27 @@
 
 int i = 0;
 void CreateFile(char path[]);
+void Cat(char filename[]);
+
 
 int main(int argc , char *argv[])
 {
     while(i < argc) {
-        if (!strcmp(argv[++i], "createfile"))
+        i++;
+        if (!strcmp(argv[i], "createfile"))
         {
             if(!strcmp(argv[++i] , "--file")) {
                 printf("Invalid Format! %s\n" , argv[i]);
                 continue;
             }
             CreateFile(argv[++i]);
+        }
+        else if(!strcmp(argv[i] , "cat")) {
+            if (!strcmp(argv[++i], "--file")) {
+                printf("Invalid Format! %s\n", argv[i]);
+                continue;
+            }
+            Cat(argv[++i]);
         }
     }
     return 0;
@@ -48,6 +58,26 @@ void CreateFile(char path[])
         temp = fopen(token, "w");
         fclose(temp);
         printf("File Successfully created\n");
+    }
+    exit(0);
+}
+
+void Cat(char filename[])
+{
+    FILE *file = fopen(filename , "r");
+    if(!file)
+        printf("There Is No Such File!\n");
+    else
+    {
+        int pointer;
+        while(1)
+        {
+            pointer = fgetc(file);
+            if(feof(file))
+                break;
+            printf("%c" , pointer);
+        }
+        fclose(file);
     }
     exit(0);
 }
