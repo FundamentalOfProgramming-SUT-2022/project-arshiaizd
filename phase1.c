a6d6bea752dfc7dda1678ed122c3d30c4d7b6929
ArshiaIzd <stevevisht@gmail.com>
2023-01-30 15:29:02 +0330

    Undo and Compare added!

--- phase1.c
+++ phase1.c
@@ -3,14 +3,19 @@
 #include <string.h>
 #include <dirent.h>
 #include <fileapi.h>
-char ReferenceFileName[1000];
-void CreateFile2(char FileName[]);
+#include <unistd.h>
+char ReferenceFileName[1000] , Filename2[1000] ,Filename3[1000];
+void CreateFile2(char FileName[] , int handle);
 void Cat(char FileName[]);
 void Removestr(char FileName[] ,int LineNum , int CharNum , int size , int fb ,int handle);
 void Insertstr(char FileName[] , FILE* tempfile , int LineNumber , int CharNum , int handle);
 void Copy(char FileName[] ,int LineNum , int CharNum , int size , int fb , int handle);
 void Cut(char FileName[] ,int LineNum , int CharNum , int size , int fb);
 void Paste(char FileName[] ,int LineNum , int CharNum);
+void UndoHelper(FILE *file);
+void Undo(char FileName[]);
+void TextComparator(char File1[] , char File2[]);
+void StringCheck(char *String);
 FILE *Reference(char FileName[]);
 
 
@@ -25,7 +30,7 @@ int main(int argc , char *argv[])
                 printf("Invalid Format! --file");
                 continue;
             }
-            CreateFile2(argv[++i]);
+            CreateFile2(argv[++i] , 0);
         }
         else if(!strcmp(argv[i] , "cat")) {
             if (!strcmp(argv[++i], "-file")) {
@@ -131,11 +136,27 @@ int main(int argc , char *argv[])
             sscanf(argv[++i] , "%d:%d" , &LineNum , &CharNum);
             Paste(argv[FilePointer] , LineNum , CharNum);
         }
+        else if (!strcmp(argv[i], "undo"))
+        {
+            if(!strcmp(argv[++i] , "-file")) {
+                printf("Invalid Format! --file");
+                continue;
+            }
+            Undo(argv[++i]);
+        }
+        else if (!strcmp(argv[i], "compare"))
+        {
+            if(!strcmp(argv[++i] , "-file")) {
+                printf("Invalid Format! --file");
+                continue;
+            }
+            TextComparator(argv[i+1] , argv[i+2]);
+        }
     }
     return 0;
 }
 
-void CreateFile2(char FileName[])
+void CreateFile2(char FileName[] , int handle)
 {
     char *token = strtok(FileName , "/");
     while(token != NULL)
@@ -165,6 +186,7 @@ void CreateFile2(char FileName[])
             exit(1);
         }
         fclose(temp);
+        if(handle) return;
         printf("File Successfully created\n");
     }
     return;
@@ -189,12 +211,15 @@ void Cat(char FileName[])
 void Removestr(char FileName[] ,int LineNum , int CharNum , int size , int fb , int handle)
 {
     int temp;
+    strcpy(Filename2 , FileName);
     FILE *file = Reference(FileName);
     FILE *destination = fopen("destination.txt", "wb+");
+    chdir("D:\\Project");
+    UndoHelper(file);
     fseek(file , 0 , SEEK_SET);
     for(int j = 1 ; j < LineNum ; j++) {
         temp = 'A';
-        while (temp != '\r') {
+        while (temp != '\n') {
             temp = fgetc(file);
             if(temp == EOF)
             {
@@ -225,6 +250,8 @@ void Removestr(char FileName[] ,int LineNum , int CharNum , int size , int fb , 
     }
     fclose(file);
     fclose(destination);
+    file = Reference(Filename2);
+    fclose(file);
     remove(ReferenceFileName);
     rename("destination.txt" , ReferenceFileName);
     if(handle)
@@ -233,16 +260,18 @@ void Removestr(char FileName[] ,int LineNum , int CharNum , int size , int fb , 
     return;
 }
 
-
 void Insertstr(char FileName[] , FILE *tempfile , int LineNum , int CharNum ,int handle)
 {
     int temp , flag = 0;
+    strcpy(Filename2 , FileName);
     FILE *file = Reference(FileName);
     FILE *destination = fopen("destination.txt", "wb+");
+    chdir("D:\\Project");
+    UndoHelper(file);
     fseek(file , 0 , SEEK_SET);
     for(int j = 1 ; j < LineNum ; j++) {
         temp = 'A';
-        while (temp != '\r') {
+        while (temp != '\n') {
             temp = fgetc(file);
             if(temp == EOF)
             {
@@ -305,12 +334,15 @@ void Insertstr(char FileName[] , FILE *tempfile , int LineNum , int CharNum ,int
             break;
         fprintf(destination, "%c", temp2);
     }
+    chdir(FileName);
     fclose(file);
     fclose(destination);
+    file = Reference(Filename2);
+    fclose(file);
     remove(ReferenceFileName);
     rename("destination.txt" , ReferenceFileName);
     if(handle) return;
-    printf("String Successfully Edited!\n");
+    printf("String Successfully inserted!\n");
     return;
 }
 
@@ -318,13 +350,14 @@ void Copy(char FileName[] ,int LineNum , int CharNum , int size , int fb ,int ha
 {
     char temp;
     FILE *file = Reference(FileName);
+    chdir("D:\\Project");
     chdir("system");
     DeleteFileA("clipboard.txt");
     FILE *clipboard = fopen("clipboard.txt", "wb+");
     fseek(file , 0 , SEEK_SET);
     for(int j = 1 ; j < LineNum ; j++) {
         temp = 'A';
-        while (temp != '\r') {
+        while (temp != '\n') {
             temp = fgetc(file);
             fflush(stdout);
             if(temp == EOF)
@@ -332,7 +365,6 @@ void Copy(char FileName[] ,int LineNum , int CharNum , int size , int fb ,int ha
                 printf("That line does not exist!!\n");
                 exit(-1);
             }
-
         }
     }
     for(int j = 0; j <= CharNum-1;j++) {
@@ -355,10 +387,10 @@ void Copy(char FileName[] ,int LineNum , int CharNum , int size , int fb ,int ha
     {
         char temp2;
         temp2 = fgetc(file);
-        if(temp == EOF)
+        if(temp2 == EOF)
         {
             printf("Pointer Reached End of File\n");
-            break;
+            exit(1);
         }
         fputc(temp2 , clipboard);
     }
@@ -372,14 +404,16 @@ void Copy(char FileName[] ,int LineNum , int CharNum , int size , int fb ,int ha
 
 void Cut(char FileName[] ,int LineNum , int CharNum , int size , int fb)
 {
+    strcpy(Filename3 , FileName);
     Copy(FileName , LineNum , CharNum , size ,fb ,1);
-    Removestr(FileName , LineNum , CharNum , size , fb ,1);
+    Removestr(Filename3 , LineNum , CharNum , size , fb ,1);
     printf("String Successfully Cut\n");
     return;
 }
 
 void Paste(char FileName[] ,int LineNum , int CharNum)
 {
+    chdir("D:\\Project");
     chdir("system");
     FILE *tempfile = fopen("clipboard.txt" , "rb+");
     chdir("..");
@@ -388,6 +422,114 @@ void Paste(char FileName[] ,int LineNum , int CharNum)
     return;
 }
 
+void Undo(char FileName[])
+{
+    FILE *file = Reference(FileName);
+    file = freopen(ReferenceFileName , "wb+" , file);
+    char temp[1000];
+    char temp2[1000];
+    sprintf(temp, "undo%s", ReferenceFileName);
+    chdir("D:\\Project");
+    chdir("system");
+    if (chdir(temp) == -1) {
+        printf("There Is No action To Undo!!\n");
+    }
+    char k;
+    char *token = strtok(temp , ".");
+    int j , Numpointer = strlen(token);
+    for(j = 0;j < Numpointer;j++)
+    {
+        temp2[j] = token[j];
+    }
+    temp2[Numpointer] = '0';
+    temp2[Numpointer+1] = '.';
+    token =strtok(NULL , ".");
+    for(j = 2;j <= strlen(token)+1;j++)
+    {
+        temp2[Numpointer + j] = token[j-2];
+    }
+    temp2[Numpointer + j] = '\0';
+    for(k = 1;;k++)
+    {
+        temp2[Numpointer] = k + '0';
+        if(access(temp2 , R_OK) == -1) {
+            break;
+        }
+    }
+    temp2[Numpointer] = k-1 + '0';
+    FILE *ToBeDeleted = fopen(temp2 , "rb+");
+    if(!ToBeDeleted)
+    {
+        printf("There is No action To Undo!\n");
+        return;
+    }
+    fseek(ToBeDeleted , 0 ,SEEK_SET);
+    while(1)
+    {
+        int tempchar = fgetc(ToBeDeleted);
+        if(tempchar == EOF) break;
+        fputc(tempchar , file);
+    }
+    fclose(ToBeDeleted);
+    fclose(file);
+    DeleteFileA(temp2);
+    chdir("D:\\Project");
+    printf("Undoed Successfully\n");
+}
+
+void TextComparator(char File1[] ,char File2[])
+{
+    FILE *file1 = Reference(File1) , *file2 = Reference(File2);
+    fseek(file1 , 0 , SEEK_SET); fseek(file2 , 0 , SEEK_SET);
+    int j = 1, flag = 0;
+    char LOF1[10000] , LOF2[10000];
+    while(1)
+    {
+        fgets(LOF1 , 10000, file1);
+        fgets(LOF2 , 10000, file2);
+        StringCheck(LOF2);
+        StringCheck(LOF1);
+        if(strcmp(LOF1 , LOF2))
+        {
+            printf("============ #%d ============\n%s\n%s\n" , j , LOF1 , LOF2);
+        }
+        j++;
+        if(feof(file1) && !feof(file2)) {
+            flag = 2;
+            break;
+        }
+        if(!feof(file1) && feof(file2)) {
+            flag = 1;
+            break;
+        }
+        if(feof(file1) && feof(file2)) {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag == 1) {
+        while (1) {
+            if (feof(file1))
+                return;
+            fgets(LOF1, 10000, file1);
+            StringCheck(LOF1);
+            printf(">>>>>>>>>>>> #%d - #%d >>>>>>>>>>>>\n%s\n", j, j, LOF1);
+            j++;
+        }
+    }
+    if(flag == 2) {
+        while (1) {
+            if (feof(file2))
+                return;
+            fgets(LOF2, 10000, file2);
+            StringCheck(LOF2);
+            printf("<<<<<<<<<<<< #%d - #%d <<<<<<<<<<<<\n%s\n", j, j, LOF2);
+            j++;
+        }
+    }
+
+
+}
 
 
 
@@ -395,6 +537,63 @@ void Paste(char FileName[] ,int LineNum , int CharNum)
 
 
 
+
+
+
+void StringCheck(char *String)
+{
+    int length = strlen(String);
+    if(String[length - 1] == '\n')
+        String[length - 1] = '\0';
+}
+void UndoHelper(FILE *file) {
+    char temp[1000];
+    char temp2[1000];
+    sprintf(temp, "undo%s", ReferenceFileName);
+    chdir("system");
+    if (chdir(temp) == -1) {
+        mkdir(temp);
+        chdir(temp);
+    }
+    char k;
+    char *token = strtok(temp , ".");
+    int j , Numpointer = strlen(token);
+    for(j = 0;j < Numpointer;j++)
+    {
+        temp2[j] = token[j];
+    }
+    temp2[Numpointer] = '0';
+    temp2[Numpointer+1] = '.';
+    token =strtok(NULL , ".");
+    for(j = 2;j <= strlen(token)+1;j++)
+    {
+        temp2[Numpointer + j] = token[j-2];
+    }
+    temp2[Numpointer + j] = '\0';
+    for(k = 1;;k++)
+    {
+        temp2[Numpointer] = k + '0';
+        if(access(temp2 , R_OK) == -1) {
+            FILE *UndoTemp = fopen(temp2 ,"w");
+            fseek(file , 0 ,SEEK_SET);
+            while(1)
+            {
+                int tempchar = fgetc(file);
+                if(tempchar == EOF) break;
+//                printf("%c" , tempchar);
+                fputc(tempchar , UndoTemp);
+            }
+            fclose(UndoTemp);
+            fseek(file , 0 , SEEK_SET);
+            SetFileAttributesA(temp2 , FILE_ATTRIBUTE_HIDDEN);
+            chdir("..");
+            sprintf(temp, "undo%s", ReferenceFileName);
+            SetFileAttributesA(temp , FILE_ATTRIBUTE_HIDDEN);
+            chdir("..");
+            break;
+        }
+    }
+}
 FILE* Reference(char FileName[])
 {
     char *token = strtok( FileName, "/");
@@ -421,7 +620,6 @@ FILE* Reference(char FileName[])
     }
     else {
         fclose(temp);
-        mkdir("BYEEe");
         printf("Invalid File Name! %s\n" , token);
         exit(-1);
     }
